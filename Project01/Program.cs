4bfaea6aa099a389629a8cc3d521182ee3cf5401
Finnley <dev.finnleygrange@icloud.com>
2024-08-13 22:43:52 +0100

    Added Identity

--- Project01/Program.cs
+++ Project01/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Project01.Areas.Identity.Data;
+using Project01.Data;
 namespace Project01
 {
     public class Program
@@ -5,6 +9,11 @@ namespace Project01
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            builder.Services.AddDefaultIdentity<Project01User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -29,6 +38,8 @@ namespace Project01
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.MapRazorPages();
 
             app.Run();
         }
